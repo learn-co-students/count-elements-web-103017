a2141b2301920b2b90c1d7f90b7e3d25348fce3f
George Gore <georgelgore@gmail.com>
2017-10-30 17:59:55 -0400

    first commit

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  hash = Hash.new(0)
+  array.inject(Hash.new(0)) do |initial, current|
+    hash[current] += 1
+    hash
+  end
+
+  # array.each do |animal|
+  #   hash[animal] += 1
+  # end
+  # hash
 end
- 
