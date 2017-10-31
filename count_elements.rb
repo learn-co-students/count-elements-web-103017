a5b26f4a7a4f01f6acd705fac709acd19253ca28
eshim12 <ellishim@gmail.com>
2017-10-30 21:24:15 -0400

    Added animals and count

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
-def count_elements(array)
-  # code goes here
+def count_elements(animals)
+  animals_hsh = Hash.new
+  animals.each do |x|
+    animals_hsh[x] = 0
+  end
+  animals.each do |x|
+    if animals_hsh.keys.include?(x)
+      animals_hsh[x] += 1
+    end
+  end
+  animals_hsh
 end
- 
