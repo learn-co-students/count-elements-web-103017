828e01225280d44b2a6c4d9caf8fa87b54eb0fda
dansbands <dansbands@hotmail.com>
2017-10-30 22:53:48 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,20 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  animal = ""
+  count = 1
+
+  array.sort!
+
+  array.each do |item|
+    if animal == item
+      count += 1
+    else
+      count = 1
+    end
+    new_hash[item] = count
+    animal = item
+  end
+  new_hash
 end
- 
