a59590f653b86bae7dbecaf35bc2156b4a0182d3
psarma89 <priyam.sarma@gmail.com>
2017-10-31 16:51:52 -0400

    Adds solution file for this

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  sum_hash = Hash.new(0)
+  array.map do |element|
+    # if sum_hash.keys.include?(element)
+    #   sum_hash[element] += 1
+    # else
+    #   sum_hash[element] = 1
+    # end
+    sum_hash.keys.include?(element) ? sum_hash[element] += 1 : sum_hash[element] = 1
+  end
+  sum_hash
 end
- 
